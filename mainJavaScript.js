502a51cdc32df8fa87ba2afc2b97d5795d8860b7
Mewhy <kurtyis.bradley@gmail.com>
2018-05-29 09:37:21 -0700

    Implement arrays
    Add simplified array system. Eliminate redundancy.

--- mainJavaScript.js
+++ mainJavaScript.js
@@ -2,93 +2,41 @@ function placeOrder(){
     itemText = '';
     costText = '';
     totalCost = 0;
-    getSize(itemText,costText,totalCost);
+    types = ['size','meat','veggies','cheese','crust','sauce']
+    getOrder(itemText,costText,totalCost,types);
 }
 
-function getSize(itemText,costText,totalCost){
-    var sizes = document.getElementsByName("size");
-    for (i = 0; i < sizes.length; i++) { 
-        if(sizes[i].checked == true){
-            itemText = itemText + sizes[i].value + "<br/>";
-            costText = costText + "$" + sizes[i].getAttribute("data-cost") + ".00" + "<br/>";
-            totalCost = totalCost + Number(sizes[i].getAttribute("data-cost"));
+function getOrder(itemText,costText,totalCost,types){
+    for (i = 0; i < types.length; i++){
+        var options = document.getElementsByName(types[i]);
+        var come = 0;
+        var cove = 0;
+        for (j = 0; j < options.length; j++) { 
+            if(options[j].checked == true){
+                itemText = itemText + options[j].value + "<br/>";
+                costText = costText + "$" + options[j].getAttribute("data-cost") + ".00" + "<br/>";
+                totalCost = totalCost + Number(options[j].getAttribute("data-cost"));
+                console.log(options[j]);
+                if(options[j].getAttribute("name") == 'meat'){
+                    come++;
+                }
+                if(options[j].getAttribute("name") == 'veggies'){
+                    cove++;
+                }
+            }
+        }
+        if(come >= 1){
+            itemText = itemText + "One complementary meat selection" + "<br/>";
+            costText = costText + "-$1.00" + "<br/>";
+            totalCost = totalCost - 1;
+        }
+        if(cove >= 1){
+            itemText = itemText + "One complementary veggie selection" + "<br/>";
+            costText = costText + "-$1.00" + "<br/>";
+            totalCost = totalCost - 1;
         }
     }
-    getMeat(itemText,costText,totalCost)
-}
-
-function getMeat(itemText,costText,totalCost){
-    var meats = document.getElementsByName("meat");
-    var commeat = 0;
-    for (i = 0; i < meats.length; i++) { 
-        if(meats[i].checked == true){
-            commeat++;
-            itemText = itemText + meats[i].value + "<br/>";
-            costText = costText + "$" + meats[i].getAttribute("data-cost") + ".00" + "<br/>";
-            totalCost = totalCost + Number(meats[i].getAttribute("data-cost"));
-        }
-    }
-    if(commeat >= 1){
-        itemText = itemText + "One Complementary Meat Selection" + "<br/>";
-        costText = costText + "-$1.00" + "<br/>";
-        totalCost = totalCost - 1;
-    }
-    getVeggies(itemText,costText,totalCost)
-}
-
-function getVeggies(itemText,costText,totalCost){
-    var veggies = document.getElementsByName("veggies");
-    var comveg = 0;
-    for (i = 0; i < veggies.length; i++) { 
-        if(veggies[i].checked == true){
-            comveg++;
-            itemText = itemText + veggies[i].value + "<br/>";
-            costText = costText + "$" + veggies[i].getAttribute("data-cost") + ".00" + "<br/>";
-            totalCost = totalCost + Number(veggies[i].getAttribute("data-cost"));
-        }
-    }
-    if(comveg >= 1){
-        itemText = itemText + "One Complementary Veggie Selection" + "<br/>";
-        costText = costText + "-$1.00" + "<br/>";
-        totalCost = totalCost - 1;
-    }
-    getCheese(itemText,costText,totalCost)
-}
-
-function getCheese(itemText,costText,totalCost){
-    var cheeses = document.getElementsByName("cheese");
-    for (i = 0; i < cheeses.length; i++) { 
-        if(cheeses[i].checked == true){
-            itemText = itemText + cheeses[i].value + "<br/>";
-            costText = costText + "$" + cheeses[i].getAttribute("data-cost") + ".00" + "<br/>";
-            totalCost = totalCost + Number(cheeses[i].getAttribute("data-cost"));
-        }
-    }
-    getCrust(itemText,costText,totalCost)
-}
-
-function getCrust(itemText,costText,totalCost){
-    var crusts = document.getElementsByName("crust");
-    for (i = 0; i < crusts.length; i++) { 
-        if(crusts[i].checked == true){
-            itemText = itemText + crusts[i].value + "<br/>";
-            costText = costText + "$" + crusts[i].getAttribute("data-cost") + ".00" + "<br/>";
-            totalCost = totalCost + Number(crusts[i].getAttribute("data-cost"));
-        }
-    }
-    getSauce(itemText,costText,totalCost)
-}
-
-function getSauce(itemText,costText,totalCost){
-    var sauces = document.getElementsByName("sauce");
-    for (i = 0; i < sauces.length; i++) { 
-        if(sauces[i].checked == true){
-            itemText = itemText + sauces[i].value + "<br/>";
-            costText = costText + "$" + sauces[i].getAttribute("data-cost") + ".00" + "<br/>";
-            totalCost = totalCost + Number(sauces[i].getAttribute("data-cost"));
-        }
-    }
-    totals(itemText,costText,totalCost)
+    totals(itemText,costText,totalCost);
 }
 
 function totals(itemText,costText,totalCost){
